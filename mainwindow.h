8fb700d6d8e56a7819ab153a3ac1b9483f34be89
ThePizzaDu62 <45798269+ThePizzaDu62@users.noreply.github.com>
2019-02-13 16:21:04 +0100

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QStringList possibleCalculations{"Périmètre", "Aire", "Volume", "Théorème"};
+    QStringList areaShapes{"Rectangle", "Carré", "Disque", "Parallèlogramme", "Triangle", "Trapèze", "Losange", "Sphère", "Cône", "Cube", "Parallélépipède rectangle", "Cylindre", "Pyramide à base carrée"};
+    QStringList perimeterShapes{"Rectangle", "Carré", "Cercle", "Parallèlogramme", "Triangle", "Trapèze", "Losange"};
+    QStringList volumeShapes{"Cube", "Parallélépipède rectangle", "Cylindre", "Cône", "Pyramide à base carrée", "Pyramide à base rectangulaire", "Sphère"};
+    QStringList theorems{"Pythagore", "Thalès"};
+    QStringList pythagoreVersion{"Hypoténuse", "Autre côté"};
+    QStringList thalesVersion{"Numérateur", "Dénominateur"};
+};
+
+#endif // MAINWINDOW_H
