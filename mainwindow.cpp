f046074962f68802340e5c82989bbec29c153eb9
ThePizzaDu62 <45798269+ThePizzaDu62@users.noreply.github.com>
2019-02-13 18:15:53 +0100

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,13 +7,35 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->calculationChoiceComboBox->addItems(possibleCalculations);
-    if(ui->calculationChoiceComboBox->currentText() == "Aire") {
-        ui->shapeLabel->setText("De quelle figure voulez vous calculer l'aire");
-        ui->shapeComboBox->addItems(areaShapes);
-    }
+    auto a = new QAction(this);
+    auto w = new QWidget(this);
+    QObject::connect(a, &QComboBox::currentTextChanged, w, &MainWindow::setListInBox));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setListInBox(const QString &arg1)
+{
+    if(arg1 == "Aire") {
+        ui->shapeComboBox->addItems(areaShapes);
+        ui->shapeLabel->setText("De quelle figure voulez vous calculer l'aire ?");
+    }
+
+    else if(arg1 == "Périmètre") {
+        ui->shapeComboBox->addItems(perimeterShapes);
+        ui->shapeLabel->setText("De quelle figure voulez vous calculer le périmètre ?");
+    }
+
+    else if(arg1 == "Volume") {
+        ui->shapeComboBox->addItems(volumeShapes);
+        ui->shapeLabel->setText("De quelle figure voulez vous calculer le volume ?");
+    }
+
+    else if(arg1 == "Théorème") {
+        ui->shapeComboBox->addItems(theorems);
+        ui->shapeLabel->setText("Quel théorème voulez vous utiliser ?");
+    }
 }
